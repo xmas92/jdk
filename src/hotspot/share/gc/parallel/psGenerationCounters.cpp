2ae6ca0f23c20bb85f5a1fe173865a143e1c53ba
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2022-11-25 12:45:20 +0000

    [ps]GenerationCounters

--- src/hotspot/share/gc/parallel/psGenerationCounters.cpp
+++ src/hotspot/share/gc/parallel/psGenerationCounters.cpp
@@ -23,6 +23,7 @@
  *
  */
 
+#include "memory/allocationManaged.hpp"
 #include "precompiled.hpp"
 #include "gc/parallel/psGenerationCounters.hpp"
 #include "memory/allocation.inline.hpp"
@@ -41,26 +42,28 @@ PSGenerationCounters::PSGenerationCounters(const char* name,
     ResourceMark rm;
 
     const char* cns = PerfDataManager::name_space("generation", ordinal);
+    // candidate: c-d
+    const size_t len = strlen(cns) + 1;
+    _name_space = make_managed_c_heap_array_default_init<char>(len, mtGC);
+    strncpy(_name_space.get(), cns, len);
+    _name_space[len-1] = '\0';
 
-    _name_space = NEW_C_HEAP_ARRAY(char, strlen(cns)+1, mtGC);
-    strcpy(_name_space, cns);
-
-    const char* cname = PerfDataManager::counter_name(_name_space, "name");
+    const char* cname = PerfDataManager::counter_name(_name_space.get(), "name");
     PerfDataManager::create_string_constant(SUN_GC, cname, name, CHECK);
 
-    cname = PerfDataManager::counter_name(_name_space, "spaces");
+    cname = PerfDataManager::counter_name(_name_space.get(), "spaces");
     PerfDataManager::create_constant(SUN_GC, cname, PerfData::U_None,
                                      spaces, CHECK);
 
-    cname = PerfDataManager::counter_name(_name_space, "minCapacity");
+    cname = PerfDataManager::counter_name(_name_space.get(), "minCapacity");
     PerfDataManager::create_constant(SUN_GC, cname, PerfData::U_Bytes,
       min_capacity, CHECK);
 
-    cname = PerfDataManager::counter_name(_name_space, "maxCapacity");
+    cname = PerfDataManager::counter_name(_name_space.get(), "maxCapacity");
     PerfDataManager::create_constant(SUN_GC, cname, PerfData::U_Bytes,
       max_capacity, CHECK);
 
-    cname = PerfDataManager::counter_name(_name_space, "capacity");
+    cname = PerfDataManager::counter_name(_name_space.get(), "capacity");
     _current_size = PerfDataManager::create_variable(SUN_GC, cname,
        PerfData::U_Bytes, _ps_virtual_space->committed_size(), CHECK);
   }

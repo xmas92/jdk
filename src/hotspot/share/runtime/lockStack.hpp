041153038478438e0f1ebe8aad07b151e39b64d8
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2023-09-25 10:01:10 +0000

    recursive lightweight + some counters and refactoring

--- src/hotspot/share/runtime/lockStack.hpp
+++ src/hotspot/share/runtime/lockStack.hpp
@@ -35,8 +35,10 @@ class OopClosure;
 
 class LockStack {
   friend class VMStructs;
-private:
+public:
   static const int CAPACITY = 8;
+
+private:
 
   // TODO: It would be very useful if JavaThread::lock_stack_offset() and friends were constexpr,
   // but this is currently not the case because we're using offset_of() which is non-constexpr,
@@ -49,7 +51,9 @@ private:
   // We do this instead of a simple index into the array because this allows for
   // efficient addressing in generated code.
   uint32_t _top;
+  bool _has_recu;
   oop _base[CAPACITY];
+  size_t _recu[CAPACITY];
 
   // Get the owning thread of this lock-stack.
   inline JavaThread* get_thread() const;
@@ -66,6 +70,8 @@ private:
 public:
   static ByteSize top_offset()  { return byte_offset_of(LockStack, _top); }
   static ByteSize base_offset() { return byte_offset_of(LockStack, _base); }
+  static ByteSize has_recu_offset() { return byte_offset_of(LockStack, _has_recu); }
+  static ByteSize recu_offset() { return byte_offset_of(LockStack, _recu); }
 
   LockStack(JavaThread* jt);
 
@@ -82,14 +88,24 @@ public:
   // Pops an oop from this lock-stack.
   inline oop pop();
 
+  // Get the oldest oop in the stack
+  inline oop bottom();
+
+  // Try recursive exit
+  inline bool try_recursive_exit(oop o);
+
+  // Try recursive enter
+  inline bool try_recursive_enter(oop o);
+
   // Removes an oop from an arbitrary location of this lock-stack.
-  inline void remove(oop o);
+  inline size_t remove(oop o);
 
   // Tests whether the oop is on this lock-stack.
   inline bool contains(oop o) const;
 
   // GC support
   inline void oops_do(OopClosure* cl);
+  inline void recursive_oops_do(OopClosure* cl);
 
 };
 

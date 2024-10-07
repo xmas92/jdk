56019a0e90e394f5660afbe141157dd6b8065960
Joel Sikström <joel.sikstrom@oracle.com>
2024-10-07 15:21:49 +0200

    ZPhysicalMemory unsorted -> append

--- src/hotspot/share/gc/z/zMappedMemory.cpp
+++ src/hotspot/share/gc/z/zMappedMemory.cpp
@@ -70,7 +70,7 @@ void ZMappedMemory::extend_mapping(const ZMappedMemory& right) {
   _vmem = ZVirtualMemory(_vmem.start(), _vmem.size() + right.size());
 
   // Combine physical memory segments in the order the appear (i.e unsorted).
-  _pmem.add_segments_unsorted(right.physical_memory());
+  _pmem.append_segments(right.physical_memory());
 }
 
 const ZVirtualMemory& ZMappedMemory::virtual_memory() const {

65d06e7fd89fdb1485abd043a67ffa63d991cf2f
Joel Sikström <joel.sikstrom@oracle.com>
2024-10-10 13:17:16 +0200

    Consistent use of "mapping" instead of "mapped" in function signatures.

--- test/hotspot/gtest/gc/z/test_zMappedCache.cpp
+++ test/hotspot/gtest/gc/z/test_zMappedCache.cpp
@@ -35,8 +35,8 @@ protected:
     ZVirtualMemory vmem2(zoffset(100), 100);
     ZPhysicalMemory pmem;
 
-    cache.free_mapped(ZMappedMemory(vmem1, pmem));
-    cache.free_mapped(ZMappedMemory(vmem2, pmem));
+    cache.free_mapping(ZMappedMemory(vmem1, pmem));
+    cache.free_mapping(ZMappedMemory(vmem2, pmem));
 
     EXPECT_EQ(cache._tree._node_count, 1);
     EXPECT_EQ(cache._tree._root->key(), zoffset(0));
@@ -51,8 +51,8 @@ protected:
     ZVirtualMemory vmem2(zoffset(100), 100);
     ZPhysicalMemory pmem;
 
-    cache.free_mapped(ZMappedMemory(vmem2, pmem));
-    cache.free_mapped(ZMappedMemory(vmem1, pmem));
+    cache.free_mapping(ZMappedMemory(vmem2, pmem));
+    cache.free_mapping(ZMappedMemory(vmem1, pmem));
 
     EXPECT_EQ(cache._tree._node_count, 1);
     EXPECT_EQ(cache._tree._root->key(), zoffset(0));
@@ -69,9 +69,9 @@ protected:
 
     ZPhysicalMemory pmem;
 
-    cache.free_mapped(ZMappedMemory(vmem1, pmem));
-    cache.free_mapped(ZMappedMemory(vmem3, pmem));
-    cache.free_mapped(ZMappedMemory(vmem2, pmem));
+    cache.free_mapping(ZMappedMemory(vmem1, pmem));
+    cache.free_mapping(ZMappedMemory(vmem3, pmem));
+    cache.free_mapping(ZMappedMemory(vmem2, pmem));
 
     EXPECT_EQ(cache._tree._node_count, 1);
     EXPECT_EQ(cache._tree._root->key(), zoffset(0));
@@ -96,22 +96,22 @@ protected:
     ZVirtualMemory vmem2(zoffset(100), 50);
     ZVirtualMemory vmem3(zoffset(200), 50);
 
-    cache.free_mapped(ZMappedMemory(vmem1, pmem1));
-    cache.free_mapped(ZMappedMemory(vmem2, pmem2));
-    cache.free_mapped(ZMappedMemory(vmem3, pmem1));
+    cache.free_mapping(ZMappedMemory(vmem1, pmem1));
+    cache.free_mapping(ZMappedMemory(vmem2, pmem2));
+    cache.free_mapping(ZMappedMemory(vmem3, pmem1));
 
-    ZMappedMemory chunk = cache.remove_mapped_contiguous(50);
+    ZMappedMemory chunk = cache.remove_mapping_contiguous(50);
     EXPECT_EQ(chunk.start(), zoffset(0));
     EXPECT_EQ(chunk.size(), (size_t)50);
 
-    chunk = cache.remove_mapped_contiguous(25);
+    chunk = cache.remove_mapping_contiguous(25);
     EXPECT_EQ(chunk.start(), zoffset(100));
     EXPECT_EQ(chunk.size(), (size_t)25);
     EXPECT_EQ(chunk.physical_memory().nsegments(), 1);
     EXPECT_EQ(chunk.physical_memory().segment(0).start(), seg1.start());
     EXPECT_EQ(chunk.physical_memory().segment(0).size(), seg1.size());
 
-    chunk = cache.remove_mapped_contiguous(100);
+    chunk = cache.remove_mapping_contiguous(100);
     EXPECT_TRUE(chunk.is_null());
   }
 
@@ -125,13 +125,13 @@ protected:
     ZVirtualMemory vmem3(zoffset(400), 100);
     ZVirtualMemory vmem4(zoffset(600), 100);
 
-    cache.free_mapped(ZMappedMemory(vmem1, pmem));
-    cache.free_mapped(ZMappedMemory(vmem2, pmem));
-    cache.free_mapped(ZMappedMemory(vmem3, pmem));
-    cache.free_mapped(ZMappedMemory(vmem4, pmem));
+    cache.free_mapping(ZMappedMemory(vmem1, pmem));
+    cache.free_mapping(ZMappedMemory(vmem2, pmem));
+    cache.free_mapping(ZMappedMemory(vmem3, pmem));
+    cache.free_mapping(ZMappedMemory(vmem4, pmem));
 
     ZArray<ZMappedMemory> mappings;
-    cache.remove_mapped(&mappings, 150);
+    cache.remove_mappings(&mappings, 150);
 
     EXPECT_EQ(mappings.length(), 2);
     EXPECT_EQ(mappings.at(0).start(), zoffset(600));

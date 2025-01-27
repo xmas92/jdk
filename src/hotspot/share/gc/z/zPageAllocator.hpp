c3223cebd021734e2df1c314ad68646cadc301bc
Joel Sikström <joel.sikstrom@oracle.com>
2025-01-27 11:45:41 +0100

    Move ZCacheState definition to .cpp-file

--- src/hotspot/share/gc/z/zPageAllocator.hpp
+++ src/hotspot/share/gc/z/zPageAllocator.hpp
@@ -40,50 +40,14 @@
 #include "gc/z/zVirtualMemory.hpp"
 
 class ThreadClosure;
-class ZGeneration;
 class ZCacheState;
+class ZGeneration;
 class ZPageAllocation;
 class ZPageAllocator;
 class ZPageAllocatorStats;
 class ZUncommitter;
 class ZUnmapper;
 class ZWorkers;
-
-class ZCacheState {
-  friend class ZPageAllocator;
-
-private:
-  ZMappedCache               _cache;
-  volatile size_t            _current_max_capacity;
-  volatile size_t            _capacity;
-  volatile size_t            _claimed;
-  volatile size_t            _used;
-  size_t                     _used_generations[2];
-  struct {
-    size_t                   _used_high;
-    size_t                   _used_low;
-  } _collection_stats[2];
-  double                     _last_commit;
-  double                     _last_uncommit;
-  size_t                     _to_uncommit;
-
-public:
-  void initialize(size_t max_capacity);
-
-  size_t available_memory() const;
-  size_t soft_max_capacity() const;
-
-  size_t increase_capacity(size_t size);
-  void decrease_capacity(size_t size, bool set_max_capacity);
-
-  void increase_used(size_t size);
-  void decrease_used(size_t size);
-
-  void increase_used_generation(ZGenerationId id, size_t size);
-  void decrease_used_generation(ZGenerationId id, size_t size);
-
-  void reset_statistics(ZGenerationId id);
-};
 
 class ZPageAllocator {
   friend class VMStructs;

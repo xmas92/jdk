7ea318f64ab30d7d9244200c128dc05520c58e40
Joel Sikström <joel.sikstrom@oracle.com>
2025-02-06 17:50:44 +0100

    TestMappedCacheHarvest small update

--- test/hotspot/jtreg/gc/z/TestMappedCacheHarvest.java
+++ test/hotspot/jtreg/gc/z/TestMappedCacheHarvest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,7 +26,7 @@ package gc.z;
 /*
  * @test TestMappedCacheHarvest
  * @requires vm.gc.Z
- * @summary Test ZGC mapped cache Harvesting
+ * @summary Test ZGC mapped cache harvesting
  * @library /test/lib
  * @run driver gc.z.TestMappedCacheHarvest
  */

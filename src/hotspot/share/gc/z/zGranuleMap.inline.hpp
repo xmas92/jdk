29e5313a93d5dbbfe319c21ae9853d52f2e7520f
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2025-02-24 15:56:36 +0000

    Enhance ZArrayIterator

--- src/hotspot/share/gc/z/zGranuleMap.inline.hpp
+++ src/hotspot/share/gc/z/zGranuleMap.inline.hpp
@@ -109,6 +109,6 @@ inline void ZGranuleMap<T>::release_put(zoffset offset, size_t size, T value) {
 
 template <typename T, bool Parallel>
 inline ZGranuleMapIterator<T, Parallel>::ZGranuleMapIterator(const ZGranuleMap<T>* granule_map)
-  : ZArrayIteratorImpl<T, Parallel>(granule_map->_map, granule_map->_size) {}
+  : ZArrayIteratorImpl<T, Parallel, true /* IsConst */>(granule_map->_map, granule_map->_size) {}
 
 #endif // SHARE_GC_Z_ZGRANULEMAP_INLINE_HPP

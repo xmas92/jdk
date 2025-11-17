2113b32e674495bfe164b45fc9af1cc16dcbda70
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2025-11-17 09:48:49 +0000

    windows aarch64: Specialize AtomicAccess::PlatformXchg<1>

--- src/hotspot/os_cpu/windows_aarch64/atomicAccess_windows_aarch64.hpp
+++ src/hotspot/os_cpu/windows_aarch64/atomicAccess_windows_aarch64.hpp
@@ -68,9 +68,6 @@ DEFINE_INTRINSIC_ADD(InterlockedAdd64, __int64)
 
 #undef DEFINE_INTRINSIC_ADD
 
-template<>
-struct AtomicAccess::PlatformXchg<1> : AtomicAccess::XchgUsingCmpxchg<1> {};
-
 #define DEFINE_INTRINSIC_XCHG(IntrinsicName, IntrinsicType)               \
   template<>                                                              \
   template<typename T>                                                    \
@@ -78,13 +75,15 @@ struct AtomicAccess::PlatformXchg<1> : AtomicAccess::XchgUsingCmpxchg<1> {};
                                                                          T exchange_value, \
                                                                          atomic_memory_order order) const { \
     STATIC_ASSERT(sizeof(IntrinsicType) == sizeof(T));                    \
-    STATIC_ASSERT(sizeof(IntrinsicType) == 4 ||                           \
+    STATIC_ASSERT(sizeof(IntrinsicType) == 1 ||                           \
+                  sizeof(IntrinsicType) == 4 ||                           \
                   sizeof(IntrinsicType) == 8);                            \
     return PrimitiveConversions::cast<T>(                                 \
       IntrinsicName(reinterpret_cast<IntrinsicType volatile *>(dest),     \
                     PrimitiveConversions::cast<IntrinsicType>(exchange_value))); \
   }
 
+DEFINE_INTRINSIC_XCHG(InterlockedExchange8,  char)
 DEFINE_INTRINSIC_XCHG(InterlockedExchange,   long)
 DEFINE_INTRINSIC_XCHG(InterlockedExchange64, __int64)
 
